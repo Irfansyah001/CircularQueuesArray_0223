68ce00229987e9fd15e042b22ec2b5ed86bd0788
Irfansyah001 <syahirfan545@gmail.com>
2024-05-30 15:18:25 +0700

    add int front position = front, rear position = rear, add mengecek apakah antrian kosong pada void display

--- CircularArray/CircularArray.cpp
+++ CircularArray/CircularArray.cpp
@@ -63,7 +63,15 @@ public:
 	}
 
 	void display() {
+		int FRONT_Position = FRONT;
+		int REAR_Position = REAR;
 
+		//Cek apakah antrian kosong
+		if (FRONT_Position == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "\nElements in the queue are...\n";
 	}
 };
 
