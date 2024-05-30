104293ad5d11741969731b6102be9722d689c530
Irfansyah001 <syahirfan545@gmail.com>
2024-05-30 15:29:24 +0700

    add Iterasi dari awal array hingga REAR pada void display

--- CircularArray/CircularArray.cpp
+++ CircularArray/CircularArray.cpp
@@ -88,6 +88,13 @@ public:
 				FRONT_Position++;
 			}
 			FRONT_Position = 0;
+
+			//Iterasi dari awal array hingga REAR
+			while (FRONT_Position <= REAR_Position) {
+				cout << queue_array[FRONT_Position] << "  ";
+				FRONT_Position++;
+			}
+			cout << endl;
 		}
 	}
 };
